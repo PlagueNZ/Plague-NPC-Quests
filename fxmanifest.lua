fdbaa6ed7fa7bf0916e8e4cebed24855f6b1ab4b
Plague <jacob@mcclure.nz>
2025-12-30 16:26:09 +1300

    1.3.5

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,13 +3,14 @@ game 'gta5'
 
 author 'Plague'
 description 'NPC Quest System'
-version '1.6.5'
+version '1.3.5'
 
 lua54 'yes'
 
 shared_scripts {
   '@ox_lib/init.lua',
   'config.lua',
+  'config_notifs.lua',
   'shared/util.lua',
 }
 
@@ -21,14 +22,17 @@ client_scripts {
   'client/npc.lua',
   'client/interaction.lua',
   'client/zones.lua',
-  'client/quests.lua',
+  'client/c_quests.lua',
+  'client/cl_boosting_jobs.lua',
   'cl_main.lua',
 }
 
 server_scripts {
   '@oxmysql/lib/MySQL.lua',
-  'server/quests.lua',
+  'server/s_quests.lua',
   'server/version.lua',
+  'server/s_boosting_jobs.lua',
+  'server/notify.lua',
   'server/s_main.lua',
 }
 
@@ -49,7 +53,12 @@ dependencies {
   'oxmysql'
 }
 
+exports {
+  'useCarHackingUSB'
+}
+
 escrow_ignore {
   'config.lua',
   'quests/*.json',
+  'fuckyou.txt'
 }
